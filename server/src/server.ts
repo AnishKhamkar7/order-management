f6cd4b9f12adaec6d55c23b3ddc37bfd82a5762b
AnishKhamkar <117926974+AnishKhamkar7@users.noreply.github.com>
2026-02-24 23:10:10 +0530

    feat(server): implement core server entry point and logging

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import http from "http";
+import app from "./app";
+import dotenv from "dotenv";
+import WebSocketService from "./lib/socket";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const httpServer = http.createServer(app);
+
+export const socket = new WebSocketService(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
