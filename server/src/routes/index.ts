71665e49d38e9d0b2e488962ce973613b2a2ad2b
AnishKhamkar <117926974+AnishKhamkar7@users.noreply.github.com>
2026-02-24 23:10:56 +0530

    feat(server): finalize api routing index

---
+++ server/src/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import auth from "./auth";
+import products from "./products";
+import orders from "./orders";
+import users from "./users";
+import { Request, Response } from "express";
+
+const router = Router();
+
+router.use("/auth", auth);
+router.use("/products", products);
+router.use("/orders", orders);
+router.use("/users", users);
+router.get("health", (_req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "API IS WORKING",
+  });
+});
+
+export default router;
