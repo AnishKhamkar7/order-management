3a8e7170d7f29093d85e5ab318533432d0a41573
AnishKhamkar <117926974+AnishKhamkar7@users.noreply.github.com>
2026-02-26 10:40:42 +0530

    added trust proxy for render

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,13 +4,13 @@ import routes from "./routes";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
+app.set("trust proxy", 1);
 app.use(cookieParser());
 
 // Simple CORS
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:3000"  ],
+    origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
     credentials: true,
   }),
 );
